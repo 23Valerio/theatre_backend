c7c92c38f1f0ee120eee4387bd7fd1cab6b642ab
Valerii Kuiovda <36048266+23Valerio@users.noreply.github.com>
2025-10-13 18:40:41 +0200

    change settings

--- project/settings.py
+++ project/settings.py
@@ -114,7 +114,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
 }
 # ONLY for development purposes !!!
